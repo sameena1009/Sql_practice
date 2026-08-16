9b4c181c7202d007c88ab003665462f9a851c263
sameena1009 <suhanasameena48@gmail.com>
2026-08-16 12:26:54 +0530

    Add files via upload

---
+++ 01_Basic_SELECT .sql
@@ -0,0 +1,51 @@
+-- Display all employee.
+SELECT* FROM employee_data;
+
+-- Display all customers.
+
+SELECT*  FROM customer_data;
+
+-- Display all sales
+SELECT* FROM sales_data;
+
+-- Show emplyee names only. 
+SELECT name FROM employee_data; 
+
+-- Show employee salary.  
+SELECT salary FROM employee_data; 
+
+-- Show department names. 
+SELECT Department FROM employee_data; 
+
+-- Show customers names. 
+SELECT  Customer_Name FROM customer_data; 
+
+-- Show customer cities.  
+SELECT City FROM customer_data;  
+
+-- Select produt names. 
+SELECT Product FROM sales_data; 
+
+-- Select qunatity sold. 
+SELECT Quantity FROM sales_data; 
+
+-- Show sales amount. 
+SELECT Product, Unit_Price FROM sales_data; 
+
+-- Show employee age 
+SELECT Age FROM employee_data; 
+
+-- Show joining dates 
+SELECT Joining_Date FROM employee_data; 
+
+-- Display employee ID and names. 
+SELECT Emp_ID,Name FROM employee_data; 
+
+-- Dislaay customer id and city. 
+SELECT Customer_ID,City FROM customer_data; 
+
+-- Display department and salary.  
+SELECT Department,Salary FROM employee_data;
+
+
+
